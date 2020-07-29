50736a509af0fb477c8eb502e065ddafa29ad8f7
Jaecheor Sim <jaecheor.Sim@readnumber.com>
2020-07-29 15:28:44 +0900

    최초생성

---
+++ GitTest/src/com/my/test/GitTest.java
@@ -0,0 +1,34 @@
+ /**
+ * <PRE>
+ *  Project : GitTest
+ *  Package : com.my.test
+ * </PRE>
+ * @brief 
+ * @file GitTest.java
+ * @date 2020. 7. 29.
+ * @author JaecheorSim
+ *  변경이력
+ *        이름	: 일자	: 근거자료		: 변경내용
+ *       -----------------------------------------------------
+ * 
+ * Copyright © 2020 Readnumber corp. all rights reserved.
+ */
+
+
+package com.my.test;
+
+/**
+ * @author JaecheorSim
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Version : 0.0.1");
+
+	}
+
+}
